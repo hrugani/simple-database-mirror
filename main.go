346e63ae5a1ee81d98a45146f4976e4fa9df9c45
Hélio Rugani Brandão <hrugani@gmail.com>
2021-04-18 03:26:22 +0000

    initial load test

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/godror/godror"
+	"github.com/jmoiron/sqlx"
+)
+
+func main() {
+
+	fmt.Println("TESTE")
+
+	dbx, err := sqlx.Connect("godror", `user="scott" password="tiger" connectString="dbhost:1521/orclpdb1"`)
+	if err != nil {
+		panic(err)
+	}
+	_ = dbx
+
+}
